01e79fc881c685c3fcace23579a094743d1be504
Christopher <christopher.n.thornton@gmail.com>
2020-11-22 23:25:22 -0700

    Add optional default value and additional attributes to parameters that modify the layout

--- REM/parameters.py
+++ REM/parameters.py
@@ -38,36 +38,92 @@ class AuditParameter:
             sys.exit(1)
 
         try:
-            editable = cdict['Editable']
+            editable = bool(int(cdict['IsEditable']))
         except KeyError:
             self.editable = True
+        except ValueError:
+            win2.popup_error('Configuration Error: rule {RULE}, Parameter {NAME}: "IsEditable" must be either 0 '
+                             '(False) or 1 (True)'.format(RULE=self.rule_name, NAME=self.name))
+            sys.exit(1)
         else:
-            self.editable = bool(editable)
+            self.editable = editable
+
+        try:
+            filterable = bool(int(cdict['IsFilterable']))
+        except KeyError:
+            self.filterable = True
+        except ValueError:
+            win2.popup_error('Configuration Error: rule {RULE}, Parameter {NAME}: "IsFilterable" must be either 0 '
+                             '(False) or 1 (True)'.format(RULE=self.rule_name, NAME=self.name))
+            sys.exit(1)
+        else:
+            self.filterable = filterable
+
+        try:
+            hidden = bool(int(cdict['IsHidden']))
+        except KeyError:
+            self.hidden = False
+        except ValueError:
+            win2.popup_error('Configuration Error: rule {RULE}, Parameter {NAME}: "IsHidden" must be either 0 '
+                             '(False) or 1 (True)'.format(RULE=self.rule_name, NAME=self.name))
+            sys.exit(1)
+        else:
+            self.hidden = hidden
+
+        try:
+            justification = cdict['Justification']
+        except KeyError:
+            self.justification = 'left'
+        else:
+            if justification not in ['right', 'left']:
+                self.justification = 'left'
+            else:
+                self.justification = justification
 
         try:
             self.alias = cdict['Alias']
         except KeyError:
             self.alias = name
 
+        try:
+            self.default = cdict['DefaultValue']
+        except KeyError:
+            self.default = None
+
         # Dynamic attributes
         self.value = self.value_raw = self.value_obj = None
 
-    def set_value(self, values):
+        self.set_value({self.element_key: self.default})
+
+    def reset_parameter(self):
+        """
+        Reset the parameter's values.
+        """
+        print('Info: rule {RULE}, parameter {PARAM}: resetting parameter value {VAL} to {DEF}'
+              .format(RULE=self.rule_name, PARAM=self.name, VAL=self.value, DEF=self.default))
+        self.set_value({self.element_key: self.default})
+
+    def set_value(self, values, by_key: bool = True):
         """
         Set the value of the parameter element from user input.
 
         Arguments:
 
-            values: dictionary of window element values.
+            values (dict): dictionary of window element values.
+
+            by_key (bool): get value of parameter from dictionary using the element key (default: True)
         """
-        try:
+        if by_key is True:
             elem_key = self.element_key
-        except KeyError:
-            print('Warning: rule {RULE}, parameter {PARAM}: no values set for parameter'
-                  .format(PARAM=self.name, RULE=self.rule_name))
-            value = ''
         else:
+            elem_key = self.name
+
+        try:
             value = values[elem_key]
+        except KeyError:
+            print('Warning: rule {RULE}, parameter {PARAM}: no values set for parameter with key {KEY}'
+                  .format(PARAM=self.name, RULE=self.rule_name, KEY=elem_key))
+            value = None
 
         self.value = self.value_raw = self.value_obj = value
 
@@ -108,7 +164,7 @@ class AuditParameterInput(AuditParameter):
     Input style parameter object.
     """
 
-    def layout(self, text_size: tuple = (None, None), size: tuple = (14, 1), padding: int = None):
+    def layout(self, text_size: tuple = (None, None), size: tuple = (14, 1), padding: int = None, default=True):
         """
         Create a layout for rule parameter element 'input'.
         """
@@ -126,14 +182,17 @@ class AuditParameterInput(AuditParameter):
 
         disabled = False if self.editable is True else True
 
+        param_value = '' if self.value is None or default is False else self.value
+
         layout = [sg.Text(desc, size=text_size, pad=((0, pad_el), 0), justification='r', font=bold_font,
                           background_color=bg_col),
-                  sg.Input('', key=key, size=size, enable_events=True, pad=((0, pad_el), 0), font=font,
+                  sg.Input(param_value, key=key, size=size, enable_events=True, pad=((0, pad_el), 0), font=font,
                            background_color=in_col, disabled=disabled,
                            tooltip=_('Input value for {}'.format(self.description))),
                   sg.Canvas(size=(padding, 0), visible=True, background_color=bg_col)]
 
-        return sg.Col([layout], pad=(0, 0), background_color=bg_col)
+#        return sg.Col([layout], pad=(0, 0), background_color=bg_col)
+        return layout
 
 
 class AuditParameterCombo(AuditParameter):
@@ -152,7 +211,7 @@ class AuditParameterCombo(AuditParameter):
 
             self.combo_values = []
 
-    def layout(self, text_size: tuple = (None, None), size: tuple = None, padding: int = None):
+    def layout(self, text_size: tuple = (None, None), size: tuple = None, padding: int = None, default=True):
         """
         Create a layout for rule parameter element 'dropdown'.
         """
@@ -168,19 +227,24 @@ class AuditParameterCombo(AuditParameter):
         key = self.element_key
         desc = '{}:'.format(self.description)
         values = self.combo_values
+        if values[0] != '':
+            values.insert(0, '')
 
         width = max([len(i) for i in values]) + 1
         size = (width, 1) if size is None else size
 
         disabled = False if self.editable is True else True
 
+        param_value = '' if self.value is None or default is False else self.value
+
         layout = [sg.Text(desc, size=text_size, pad=((0, pad_el), 0), justification='r', font=bold_font,
                           background_color=bg_col),
-                  sg.Combo(values, key=key, size=size, pad=(0, 0), font=font, background_color=in_col,
-                           enable_events=True, disabled=disabled),
+                  sg.Combo(values, default_value=param_value, key=key, size=size, pad=(0, 0), font=font,
+                           background_color=in_col, enable_events=True, disabled=disabled),
                   sg.Canvas(size=(padding, 0), visible=True, background_color=bg_col)]
 
-        return sg.Col([layout], pad=(0, 0), background_color=bg_col)
+#        return sg.Col([layout], pad=(0, 0), background_color=bg_col)
+        return layout
 
 
 class AuditParameterDate(AuditParameter):
@@ -198,7 +262,7 @@ class AuditParameterDate(AuditParameter):
             win2.popup_notice(msg)
             self.format = "%Y-%m-%d"
 
-    def layout(self, text_size: tuple = (None, None), size: tuple = (14, 1), padding: int = None):
+    def layout(self, text_size: tuple = (None, None), size: tuple = (14, 1), padding: int = None, default=True):
         """
         Layout for the rule parameter element 'date'.
         """
@@ -216,20 +280,23 @@ class AuditParameterDate(AuditParameter):
 
         disabled = False if self.editable is True else True
 
+        param_value = '' if self.value is None or default is False else self.value
+
         input_key = self.element_key
         date_key = lo.as_key('{RULE} Parameter {NAME} Button'.format(RULE=self.rule_name, NAME=self.name))
         layout = [sg.Text(desc, size=text_size, pad=((0, pad_el), 0), justification='r', font=bold_font,
                           background_color=bg_col),
-                  sg.Input('', key=input_key, size=size, enable_events=True, pad=((0, pad_el), 0), font=font,
+                  sg.Input(param_value, key=input_key, size=size, enable_events=True, pad=((0, pad_el), 0), font=font,
                            background_color=in_col, disabled=disabled,
                            tooltip=_('Input date as YYYY-MM-DD or use the calendar button to select the date')),
                   sg.CalendarButton('', key=date_key, format='%Y-%m-%d', image_data=date_ico, pad=(0, 0), font=font,
                                     border_width=0, disabled=disabled, tooltip=_('Select date from calendar menu')),
                   sg.Canvas(size=(padding, 0), visible=True, background_color=bg_col)]
 
-        return sg.Col([layout], pad=(0, 0), background_color=bg_col)
+#        return sg.Col([layout], pad=(0, 0), background_color=bg_col)
+        return layout
 
-    def set_value(self, values):
+    def set_value(self, values, by_key: bool = True):
         """
         Set the value of the parameter element from user input.
 
@@ -239,30 +306,31 @@ class AuditParameterDate(AuditParameter):
         """
         dparse = dateutil.parser.parse
 
-        elem_key = self.element_key
+        if by_key is True:
+            elem_key = self.element_key
+        else:
+            elem_key = self.name
 
         try:
             value_raw: str = values[elem_key]
         except KeyError:
-            print('Warning: rule {RULE}, parameter {PARAM}: no values set for parameter'
-                  .format(PARAM=self.name, RULE=self.rule_name))
-            value_fmt = ''
-            value_raw = ''
+            print('Warning: rule {RULE}, parameter {PARAM}: no values set for parameter with element key {KEY}'
+                  .format(PARAM=self.name, RULE=self.rule_name, KEY=elem_key))
+            value_fmt = None
+            value_raw = None
+            date = None
         else:
             try:
                 date = dparse(value_raw, yearfirst=True)
-            except ValueError:
-                value_fmt = ''
-                value_raw = ''
-                date = ''
+            except (ValueError, TypeError):
+                value_fmt = value_raw = date = None
             else:
                 try:
                     value_fmt: str = date.strftime(self.format)
                 except ValueError:
                     print('Configuration Error: rule {RULE}, parameter {PARAM}: invalid format string {STR}'
                           .format(RULE=self.rule_name, PARAM=self.name, STR=self.format))
-                    value_fmt = None
-                    date = ''
+                    value_fmt = date = None
 
         self.value = value_fmt
         self.value_raw = value_raw
@@ -270,98 +338,16 @@ class AuditParameterDate(AuditParameter):
 
     def values_set(self):
         """
-        Check whether all values attributes have been set with correct
-        formatting.
+        Check whether all values attributes have been set with correct formatting.
         """
         value = self.value_raw
 
-        input_date = value.replace('-', '')
+        try:
+            input_date = value.split()[0].replace('-', '')
+        except AttributeError:
+            return False
         if input_date and len(input_date) == 8:
             return True
         else:
             return False
 
-
-class AuditParameterDateRange(AuditParameterDate):
-    """
-    Date range parameter element object.
-    """
-
-    def __init__(self, rule_name, name, cdict):
-        super().__init__(rule_name, name, cdict)
-        self.element_key2 = lo.as_key('{} {} 2'.format(rule_name, name))
-        self.value2 = None
-
-    def layout(self):
-        """
-        Layout for the rule parameter element 'date' and 'date_range'.
-        """
-        pad_el = const.ELEM_PAD
-        pad_h = const.HORZ_PAD
-        date_ico = const.CALENDAR_ICON
-        font = const.MID_FONT
-        bold_font = const.BOLD_FONT
-        in_col = const.INPUT_COL
-        bg_col = const.ACTION_COL
-
-        desc = self.description
-
-        desc_from = '{} From:'.format(desc)
-        desc_to = '{} To:'.format(desc)
-        key_from = self.element_key
-        key_to = self.element_key2
-
-        disabled = False if self.editable is True else True
-
-        layout = [[sg.Text(desc_from, pad=((0, pad_el), 0), font=bold_font, background_color=bg_col),
-                   sg.Input('', key=key_from, size=(16, 1), enable_events=True, pad=((0, pad_el), 0), font=font,
-                            background_color=in_col, disabled=disabled,
-                            tooltip=_('Input date as YYYY-MM-DD or use the calendar button to select the date')),
-                   sg.CalendarButton('', format='%Y-%m-%d', image_data=date_ico, border_width=0, pad=(0, 0),
-                                     disabled=disabled, tooltip=_('Select date from calendar menu')),
-                   sg.Text(desc_to, pad=((pad_h, pad_el), 0), font=bold_font, background_color=bg_col),
-                   sg.Input('', key=key_to, size=(16, 1), enable_events=True, pad=((0, pad_el), 0),
-                            background_color=in_col, disabled=disabled,
-                            tooltip=_('Input date as YYYY-MM-DD or use the calendar button to select the date')),
-                   sg.CalendarButton('', format='%Y-%m-%d', image_data=date_ico, border_width=0, pad=(0, 0),
-                                     disabled=disabled, tooltip=_('Select date from calendar menu')),
-                   sg.Canvas(size=(pad_h, 1), visible=True, background_color=bg_col)]]
-
-        return sg.Col(layout, pad=(0, 0), background_color=bg_col)
-
-    def filter_statement(self, table=None):
-        """
-        Generate the filter clause for SQL querying.
-        """
-        if table:
-            db_field = '{}.{}'.format(table, self.name)
-        else:
-            db_field = self.name
-
-        params = (self.value, self.value2)
-        statement = ('{} BETWEEN ? AND ?'.format(db_field), params)
-
-        return statement
-
-    def set_value(self, values):
-        """
-        Set the value of the parameter element from user input.
-
-        Arguments:
-
-            values: dictionary of window element values.
-        """
-        elem_key = self.element_key
-        elem_key2 = self.element_key2
-        self.value = values[elem_key]
-
-        self.value2 = values[elem_key2]
-
-    def values_set(self):
-        """
-        Check whether all values attributes have been set.
-        """
-        if self.value and self.value2:
-            return True
-        else:
-            return False
