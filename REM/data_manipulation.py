0b2c60ad67dfebb85be05738187870777a7bd019
Christopher <christopher.n.thornton@gmail.com>
2020-11-22 23:40:51 -0700

    Add method for generic formatting of display tables

--- REM/data_manipulation.py
+++ REM/data_manipulation.py
@@ -3,8 +3,10 @@ REM function for manipulating data.
 """
 
 import datetime
+import dateutil
 import numpy as np
 import pandas as pd
+from REM.config import settings
 import re
 
 
@@ -184,6 +186,60 @@ def fill_na(df, colname=None):
                   .format(COL=column, DTYPE=dtype))
 
     return df
+
+
+def format_display_table(dataframe, display_map, date_fmt: str = None, aliases: dict = None):
+    """
+    Format dataframe for displaying as a table.
+    """
+    relativedelta = dateutil.relativedelta.relativedelta
+    strptime = datetime.datetime.strptime
+    is_float_dtype = pd.api.types.is_float_dtype
+    is_datetime_dtype = pd.api.types.is_datetime64_any_dtype
+
+    aliases = aliases if aliases is not None else {}
+    date_fmt = date_fmt if date_fmt is not None else settings.format_date_str(date_str=settings.display_date_format)
+
+    display_header = list(display_map.keys())
+
+    # Localization specific options
+    date_offset = settings.get_date_offset()
+
+    display_df = pd.DataFrame()
+
+    # Subset dataframe by specified columns to display
+    for col_name in display_map:
+        mapped_col = display_map[col_name]
+
+        col_to_add = dataframe[mapped_col].copy()
+        dtype = col_to_add.dtype
+        print('the datatype of column {} is {}'.format(col_name, dtype))
+        if is_float_dtype(dtype):
+            col_to_add = col_to_add.apply('{:,.2f}'.format)
+        elif is_datetime_dtype(dtype):
+            print('column {} is a datetime object'.format(col_name))
+            col_to_add = col_to_add.apply(lambda x: (strptime(x.strftime(date_fmt), date_fmt) +
+                                                     relativedelta(years=+date_offset)).strftime(date_fmt)
+            if pd.notnull(x) else '')
+        display_df[col_name] = col_to_add
+
+    # Map column values to the aliases specified in the configuration
+    for alias_col in aliases:
+        alias_map = aliases[alias_col]  # dictionary of mapped values
+
+        if alias_col not in display_header:
+            print('Warning: alias {ALIAS} not found in the list of display columns'.format(ALIAS=alias_col))
+            continue
+
+        print('Info: applying aliases {MAP} to {COL}'.format(MAP=alias_map, COL=alias_col))
+
+        try:
+            display_df[alias_col].replace(alias_map, inplace=True)
+        except KeyError:
+            print('Warning: alias {ALIAS} not found in the list of display columns'.format(ALIAS=alias_col))
+            continue
+
+    return display_df
 
 
 def subset_dataframe(df, subset_rule):
